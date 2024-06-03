a0b2004316a4e042edcca3bc4e3f0d64a492da55
qndn3tp <qndn3tp@naver.com>
2024-06-03 09:42:31 +0900

    Remove

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:hoho_keypad/screens/home/attendance_button.dart';
-import 'package:hoho_keypad/screens/management/login.dart';
-import 'package:hoho_keypad/style.dart';
 import 'package:hoho_keypad/widgets/logo.dart';
 
 //////////////////////////////
@@ -16,16 +13,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: IconButton(
-          onPressed: (){
-            Get.to(
-              LoginScreen(),
-              transition: Transition.cupertino,
-              duration: const Duration(milliseconds: 500),
-            );
-          }, 
-          icon: const Icon(Icons.person_rounded, color: PrimaryColors.grey3,)),
+        backgroundColor: Colors.transparent
       ),
       body: Center(
         child: Column(
